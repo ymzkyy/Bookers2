5ac8c8bdbd80032d2fccd16dab7e695c611bed08
ymzkyy <110673799+ymzkyy@users.noreply.github.com>
2022-10-01 14:54:48 +0000

    book indexの左ページ途中まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,25 @@
 class BooksController < ApplicationController
- def index
+ 
+ def new
+  @book = Book.new
+ end
+ 
+ def index  
  end
 
 
 
-def create
 
+def create
+    @users = User.new(user_params)
+    @users.user_id = current_user.id
+    @users.save
+    redirect_to user_path
 end
+
 private
+
+ def users_params
+    params.require(:users).permit(:shop_name, :image, :caption)
+ end
 end
